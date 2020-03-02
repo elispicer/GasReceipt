918a6f92215a2fc4daf4c635ccc91af8ee0359c7
Eli Spicer <joshua.spicer.2020@student.lhschools.org>
2020-03-02 12:05:34 -0500

    Intellij commit

---
+++ src/com/LickingHeights/Main.java
@@ -0,0 +1,27 @@
+package com.LickingHeights;
+
+public class Main {
+
+    public static void main(String[] args) {
+       System.out.println("------------------------------");
+       System.out.println("|            7/11            |");
+       System.out.println("|        Sales Receipt       |");
+       System.out.println("|                            |");
+       System.out.println("|7572 Maple Lane Pataskala OH|");
+       System.out.println("|                            |");
+       System.out.println("| Date: 1/07/20 Time: 6;00PM |");
+       System.out.println("|                            |");
+       System.out.println("| Invoice#  187354748        |");
+       System.out.println("| Auth#                      |");
+       System.out.println("| Mastercard account number: |");
+       System.out.println("|     ****************56     |");
+       System.out.println("|                            |");
+       System.out.println("|Pump-1 Product-UNLD $/G-$2.9|");
+       System.out.println("|                            |");
+       System.out.println("|Gallons-20.69   Total-$60.00|");
+       System.out.println("|                            |");
+       System.out.println("|         Thank You!         |");
+       System.out.println("------------------------------");
+        }
+    }
+
